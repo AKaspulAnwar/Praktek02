ab28a0dc2f8787c51137479d5ced07474f864b1f
AKaspulAnwar <32286210+AKaspulAnwar@users.noreply.github.com>
2018-04-01 21:05:59 +0800

    Membuat Atribute dan Behavior

---
+++ src/praktek02/BeliBeras.java
@@ -0,0 +1,12 @@
+package praktek02;
+public class BeliBeras {
+    double hargaberas;
+    double bayar;
+    
+    void cetakInfo(){
+        System.out.println("===========");
+        System.out.println("Harga Beras/5kg = "+hargaberas);
+        System.out.println("Bayar           = "+bayar);
+        System.out.println("===========");
+    }
+}
