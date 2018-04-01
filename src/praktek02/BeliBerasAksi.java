c8b7eab1045cc2ecf29387330ffb3ec44f7972be
AKaspulAnwar <32286210+AKaspulAnwar@users.noreply.github.com>
2018-04-01 21:15:54 +0800

    Mengisi Nilai Atribut

--- src/praktek02/BeliBerasAksi.java
+++ src/praktek02/BeliBerasAksi.java
@@ -2,7 +2,10 @@ package praktek02;
 public class BeliBerasAksi {
     public static void main(String[] args) {
         BeliBeras siam = new BeliBeras();
+        siam.hargaberas = 50000;
+        siam.bayar      = 100000;
+        
         
     }
-    
+     
 }
